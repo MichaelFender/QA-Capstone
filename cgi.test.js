c992ca5d7011391087a6854f57e79e1a93e14024
MichaelFeneder <michael.a.fender@outlook.com>
2022-01-19 13:17:47 -0600

    industries and insights

--- cgi.test.js
+++ cgi.test.js
@@ -6,7 +6,7 @@ jest.setTimeout(30000)
 
 const driver = new Builder().withCapabilities(Capabilities.chrome()).build()
 
-test("CGI", async () => {
+test("CGI-Services", async () => {
   
     await driver.get('https://cgi.com/')
 
@@ -17,15 +17,45 @@ test("CGI", async () => {
 
     await cookie.click()
 
-    let hub = await driver.findElement(By.xpath('/html/body/header/div/div[3]/div/nav/ul/li[1]/a'))
+    let services = await driver.findElement(By.xpath('/html/body/header/div/div[3]/div/nav/ul/li[1]/a'))
 
-    await hub.click()
+    await services.click()
 
     let home = await driver.findElement(By.css('#Calque_1'))
 
     await home.click()
+})    
 
-    // await driver.sleep(5000)
+test("CGI-Industries", async () => {
+  
+    await driver.get('https://cgi.com/')
+
+    driver.manage().window().maximize();
+
+
+    let industries = await driver.findElement(By.xpath('//*[@id="main-nav"]/div/nav/ul/li[2]/a'))
+
+    await industries.click()
+
+    let home = await driver.findElement(By.css('#Calque_1'))
+
+    await home.click()
+})    
+
+test("CGI-Insights", async () => {
+  
+    await driver.get('https://cgi.com/')
+
+    driver.manage().window().maximize();
+
+
+    let insights = await driver.findElement(By.xpath('/html/body/header/div/div[3]/div/nav/ul/li[3]/a'))
+
+    await insights.click()
+
+    let home = await driver.findElement(By.css('#Calque_1'))
+
+    await home.click()
 })    
 
 //npx jest github.test.js
