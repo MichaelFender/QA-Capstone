0c435884b1f669b341bb8a3a194413120f1780ce
MichaelFeneder <michael.a.fender@outlook.com>
2022-01-19 10:48:20 -0600

    services dropdown test

--- cgi.test.js
+++ cgi.test.js
@@ -6,53 +6,26 @@ jest.setTimeout(30000)
 
 const driver = new Builder().withCapabilities(Capabilities.chrome()).build()
 
-test("github", async () => {
+test("CGI", async () => {
   
-    await driver.get('https://github.com/')
+    await driver.get('https://cgi.com/')
 
     driver.manage().window().maximize();
 
-    let hub = await driver.findElement(By.xpath('/html/body/div[1]/header/div/div[2]/div[2]/div[2]/a'))
+
+    let cookie = await driver.findElement(By.css('#popup-buttons > button.decline-button.eu-cookie-compliance-secondary-button'))
+
+    await cookie.click()
+
+    let hub = await driver.findElement(By.xpath('/html/body/header/div/div[3]/div/nav/ul/li[1]/a'))
 
     await hub.click()
 
-    let oneHub = await driver.findElement(By.css('[name="login"]'))
+    let home = await driver.findElement(By.css('#Calque_1'))
 
-    await oneHub.sendKeys("MichaelFender")
+    await home.click()
 
-    let pass = await driver.findElement(By.css('[name="password"]'))
-
-    await pass.sendKeys("10Columbine99#")
-
-    let cl = await driver.findElement(By.css('[name="commit"]'))
-
-    await cl.click()
-
-    let cd = await driver.findElement(By.xpath('(//span[@class="dropdown-caret"])[2]'))
-
-    await cd.click()
-    //END
-
-//     let yourProfileButton = await driver.findElement(By.xpath('/html/body/div[1]/header/div[7]/details/details-menu/a[1]'))
-
-//    await yourProfileButton.click()
-//    await driver.sleep(2000)
-
-    await driver.get('https://github.com/michaelfender/')
-
-
-    let editProfile = await driver.findElement(By.xpath('//*[@id="js-pjax-container"]/div[2]/div/div[1]/div/div[2]/div[3]/div[2]/div[2]/button'))
-
-    await editProfile.click()
-
-    let textArea = await driver.findElement(By.xpath('//*[@id="user_profile_bio"]'))
-    await textArea.sendKeys('Future QA analyst')
-    await driver.sleep(2000)
-
-    let saveBtn = await driver.findElement(By.xpath('//*[@id="js-pjax-container"]/div[2]/div/div[1]/div/div[2]/div[3]/div[1]/form/div[7]/button[1]'))
-    await saveBtn.click()
-    await driver.sleep(5000)
-
+    // await driver.sleep(5000)
 })    
 
 //npx jest github.test.js
