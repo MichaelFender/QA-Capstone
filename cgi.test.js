c1f39953d3959c6dce782886eef119b55a069388
MichaelFeneder <michael.a.fender@outlook.com>
2022-01-20 11:13:23 -0600

    adding await driver.sleep to pause tests

--- cgi.test.js
+++ cgi.test.js
@@ -26,7 +26,7 @@ test("CGI-Services", async () => {
     let services = await driver.findElement(By.xpath('/html/body/header/div/div[3]/div/nav/ul/li[1]/a'))
 
     await services.click()
-    await driver.sleep(2000)
+    await driver.sleep(1000)
     // => Back to homepage
     // let home = await driver.findElement(By.css('#Calque_1'))
 
@@ -48,7 +48,7 @@ test("CGI-Industries", async () => {
     let industries = await driver.findElement(By.xpath('//*[@id="main-nav"]/div/nav/ul/li[2]/a'))
 
     await industries.click()
-    await driver.sleep(2000)
+    await driver.sleep(1000)
 
     // => Back to homepage
     // let home = await driver.findElement(By.css('#Calque_1'))
@@ -70,6 +70,7 @@ test("CGI-Insights", async () => {
     let insights = await driver.findElement(By.xpath('/html/body/header/div/div[3]/div/nav/ul/li[3]/a'))
 
     await insights.click()
+    await driver.sleep(1000)
 
     // => Back to homepage
     // let home = await driver.findElement(By.css('#Calque_1'))
@@ -91,6 +92,7 @@ test("CGI-Careers", async () => {
     let careers = await driver.findElement(By.xpath('/html/body/header/div/div[3]/div/nav/ul/li[4]/a'))
 
     await careers.click()
+    await driver.sleep(1000)
 
     // => Back to homepage
     // let home = await driver.findElement(By.css('#Calque_1'))
@@ -111,6 +113,7 @@ test("CGI-Mergers", async () => {
     let mergers = await driver.findElement(By.xpath('/html/body/header/div/div[3]/div/nav/ul/li[5]/a'))
 
     await mergers.click()
+    await driver.sleep(1000)
 
     // => Back to homepage
     // let home = await driver.findElement(By.css('#Calque_1'))
@@ -132,10 +135,12 @@ test("CGI-Contact", async () => {
 
     await contact.click()
 
+
     // => Back to homepage
     let home = await driver.findElement(By.css('#Calque_1'))
 
     await home.click()
+    // await driver.sleep(1000)
     // <= ^^
 }) 
 
@@ -151,11 +156,13 @@ test("CGI-Global", async () => {
     let global = await driver.findElement(By.xpath('/html/body/header/div/div[4]/div/nav/div[2]/a'))
 
     await global.click()
+  
 
     // => Back to homepage
     let home = await driver.findElement(By.css('#Calque_1'))
 
     await home.click()
+    await driver.sleep(1000)
     // <= ^^
 }) 
 
@@ -171,13 +178,16 @@ test("CGI-About Us Video", async () => {
     let aboutUs = await driver.findElement(By.xpath('/html/body/div[3]/div/footer/div/div/div[2]/div/div/div[3]/div/nav/ul/li[1]/a'))
 
     await aboutUs.click()
+    await driver.sleep(1000)
 
     await driver.switchTo().frame(0)
     await driver.findElement(By.css(".ytp-large-play-button")).click()
+    await driver.sleep(5000)
 
     // => Back to homepage
     await driver.switchTo().defaultContent()
     await driver.findElement(By.id("Calque_1")).click()
+    await driver.sleep(1000)
     // <= ^^
 }) 
 
@@ -194,6 +204,7 @@ test("CGI-Client Stories", async () => {
     let clientStories = await driver.findElement(By.xpath('/html/body/div[3]/div/div[2]/div/section/div/div/div/section[2]/div/h2/a'))
 
     await clientStories.click()
+    await driver.sleep(1000)
 
     // => Back to homepage
     let home = await driver.findElement(By.css('#Calque_1'))
